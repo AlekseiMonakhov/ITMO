1c293172beb1840c5c64e704d93d15f1515b7075
nath <80720836+Ken-Dra@users.noreply.github.com>
2021-12-19 21:59:06 +0300

    Delete practiceJS_6_4.js

--- practiceJS_6_4.js
+++
@@ -1,9 +0,0 @@
-function setStyle(fontColor, fontSize) {
-    let nodes = document.getElementsByClassName('listNumbers')[0].childNodes;
-    nodes.forEach(item => {
-        if (item.nodeType == 1) {
-            item.style.color = fontColor;
-            item.style.fontSize = fontSize;      
-        }
-    });
-}
